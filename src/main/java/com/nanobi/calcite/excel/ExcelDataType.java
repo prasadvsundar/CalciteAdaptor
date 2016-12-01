a1ea9c0088d0c7b6459c5fa9a7449840c0439f29
prasadnanobi <prasad.vs@nanobianalytics.com>
2016-12-01 16:07:12 +0530

    Excel and CSV Double data type column support

--- src/main/java/com/nanobi/calcite/excel/ExcelDataType.java
+++ src/main/java/com/nanobi/calcite/excel/ExcelDataType.java
@@ -31,101 +31,103 @@ public class ExcelDataType {
 		List<RelDataType> typesList = new ArrayList<>();
 		List<String> names = new ArrayList<>();
 		List<ExcelFieldType> ExcelFieldTypes = new ArrayList<>();
-		
+
 		RelDataType[] types;
-		int counter=0;
-		while(rows.hasNext()){
-			 Row row = rows.next();
-	    	 Iterator<Cell> cellIterator = row.cellIterator();
-	    	  while (cellIterator.hasNext()){
-	    		Cell cell = cellIterator.next();
+		int counter = 0;
+		while (rows.hasNext()) {
+			Row row = rows.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
 				names.add(cell.getStringCellValue());
 			}
-	    	break;
-	      }
+			break;
+		}
 		types = new RelDataType[names.size()];
-		int rowCount=1;
-		while(rows.hasNext()&&checkArrayHasNullValue(types)&&rowCount<=MAX_CHECK_ROW_COUNT){
-			 Row row = rows.next();
-	    	 Iterator<Cell> cellIterator = row.cellIterator();
-	    	  while (cellIterator.hasNext()){
-	    		Cell cell = cellIterator.next();
+		int rowCount = 1;
+		while (rows.hasNext() && checkArrayHasNullValue(types)
+				&& rowCount <= MAX_CHECK_ROW_COUNT) {
+			Row row = rows.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
 				ExcelFieldType fieldType = ExcelFieldType.of(cell);
 				RelDataType type;
 				if (fieldType == null) {
-      	          type = typeFactory.createJavaType(String.class);
-      	        } else {
-      	          type = fieldType.toType(typeFactory);
-      	        }
-                if(types[cell.getColumnIndex()]==null){
-                	types[cell.getColumnIndex()]=type;
-                	//ExcelFieldTypes.add(fieldType);
-                }
+					type = typeFactory.createJavaType(String.class);
+				} else {
+					type = fieldType.toType(typeFactory);
+				}
+				if (types[cell.getColumnIndex()] == null) {
+					types[cell.getColumnIndex()] = type;
+					// ExcelFieldTypes.add(fieldType);
+				}
 			}
-	    	rowCount++;
-	      }
-		convertAllNullToStringType(types,typeFactory);
-		ExcelFieldTypes=convertTypesToExcelFieldType(types);
+			rowCount++;
+		}
+		convertAllNullToStringType(types, typeFactory);
+		ExcelFieldTypes = convertTypesToExcelFieldType(types);
 		typesList = Arrays.asList(types);
 		columnData.put("columnDataTyeps", typesList);
 		columnData.put("columnNames", names);
 		columnData.put("excelFieldTypes", ExcelFieldTypes);
 		return columnData;
 	}
-	
-	boolean checkArrayHasNullValue(RelDataType[] array){
-		for(RelDataType obj:array){
-			if(obj==null){
+
+	boolean checkArrayHasNullValue(RelDataType[] array) {
+		for (RelDataType obj : array) {
+			if (obj == null) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	void convertAllNullToStringType(RelDataType[] array,JavaTypeFactory typeFactory){
-		for(int i=0;i<array.length;i++){
-			if(array[i]==null){
+
+	void convertAllNullToStringType(RelDataType[] array,
+			JavaTypeFactory typeFactory) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == null) {
 				RelDataType type = typeFactory.createJavaType(String.class);
-				array[i]=type;
+				array[i] = type;
 			}
 		}
 	}
-	
-	List<ExcelFieldType> convertTypesToExcelFieldType(RelDataType[] types){
+
+	List<ExcelFieldType> convertTypesToExcelFieldType(RelDataType[] types) {
 		List<ExcelFieldType> excelFieldType = new ArrayList<>();
-		for(int i=0;i<types.length;i++){
+		for (int i = 0; i < types.length; i++) {
 			excelFieldType.add(ExcelFieldType.of(types[i].getFullTypeString()));
 		}
 		return excelFieldType;
 	}
-	
+
 	public Object getValue(Cell cell) {
 
 		switch (cell.getCellType()) {
-		
+
 		case Cell.CELL_TYPE_NUMERIC: {
-			try{
-				if(cell.getStringCellValue().contains(".")){
+			try {
+				if (cell.getStringCellValue().contains(".")) {
 					return Double.parseDouble(cell.getStringCellValue());
-				}else{
+				} else {
 					return cell.getNumericCellValue();
 				}
-			}catch(Exception e1){
+			} catch (Exception e1) {
 				return cell.getNumericCellValue();
 			}
 		}
-		
-		case Cell.CELL_TYPE_BOOLEAN:{
+
+		case Cell.CELL_TYPE_BOOLEAN: {
 			return cell.getBooleanCellValue();
-		
+
 		}
-		
-		case Cell.CELL_TYPE_STRING:{
-			
+
+		case Cell.CELL_TYPE_STRING: {
+
+		}
+			return cell;
 		}
 		return cell;
 	}
-		return cell;
-	}
-	
+
 }
